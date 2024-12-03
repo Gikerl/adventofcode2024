5a93c7616589cbe8b63fe354d6717e355d46d465
Gikerl <florian.haenel@live.de>
2024-12-03 11:27:25 +0100

    Day00: BoilerPlate Code

---
+++ AdventOfCode2024/Day00/Day00.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace AdventOfCode2024.Day01;
+
+public class Day00
+{
+    public void Run(bool silent)
+    {
+        var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Day00/Input.txt");
+        var lines = File.ReadAllLines(path);
+
+        var output = "";
+        if (!silent) 
+            Console.WriteLine(output);
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode2024.Day01;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace AdventOfCode2024
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
+            var task = new Day03();
+            task.Run(false);
+
+            void BenchmarkAction()
+            {
+                var benchMarkTask = new Day03();
+                benchMarkTask.Run(true);
+            }
+            Benchmark(BenchmarkAction, 10000);
+        }
+
+        private static void Benchmark(Action act, int iterations)
+        {
+            GC.Collect();
+            var stopwatch = Stopwatch.StartNew();
+            for (var i = 0; i < iterations; i++)
+                act.Invoke();
+            stopwatch.Stop();
+            Console.WriteLine($"Average execution time over {iterations} runs: {(double)stopwatch.ElapsedMilliseconds / iterations:F3} ms: ");
+        }
+    }
+}
